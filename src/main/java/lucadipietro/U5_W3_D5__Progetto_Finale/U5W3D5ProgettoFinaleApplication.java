eff08a61682df43e0c2ab8b2a24bd0f47824916a
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-05 10:57:37 +0200

    Set up project

---
+++ src/main/java/lucadipietro/U5_W3_D5__Progetto_Finale/U5W3D5ProgettoFinaleApplication.java
@@ -0,0 +1,13 @@
+package lucadipietro.U5_W3_D5__Progetto_Finale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5ProgettoFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5ProgettoFinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucadipietro/U5_W3_D5__Progetto_Finale/U5W3D5ProgettoFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package lucadipietro.U5_W3_D5__Progetto_Finale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ProgettoFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
